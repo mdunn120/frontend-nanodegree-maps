d58109dcc49b020f8406c7f1bf96a9295f7976d6
Mariah Dunn <mdunn120@gmail.com>
2016-07-03 16:53:46 -0700

    Map is working

--- js/app.js
+++ js/app.js
@@ -11,12 +11,10 @@ ko.applyBindings(new AppViewModel());
 
 //js for the map
 var map;
-function initMap(){
-	//Constrcutor creates a new map -only center and zoom are requried
-	map =  new google.maps.Map(document.getElementByID('map'), {
-		center: {lat: 40.7413549, lng: -73.9980244},
-		zoom: 13
-	});
-
+function initMap() {
+// Constructor creates a new map - only center and zoom are required.
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 40.7413549, lng: -73.9980244},
+  zoom: 13
+});
 }
-initMap();
