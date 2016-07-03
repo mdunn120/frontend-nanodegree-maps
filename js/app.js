3fa16aa202bd43de98cd766d9268c3fe13755cab
Mariah Dunn <mdunn120@gmail.com>
2016-07-03 16:16:18 -0700

    Map isn't working

--- js/app.js
+++ js/app.js
@@ -9,3 +9,14 @@ function AppViewModel() {
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
 
+//js for the map
+var map;
+function initMap(){
+	//Constrcutor creates a new map -only center and zoom are requried
+	map =  new google.maps.Map(document.getElementByID('map'), {
+		center: {lat: 40.7413549, lng: -73.9980244},
+		zoom: 13
+	});
+
+}
+initMap();
