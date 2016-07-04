c086bee4a4304f81df84a7933920d629d1bfeabd
Mariah Dunn <mdunn120@gmail.com>
2016-07-03 19:34:09 -0700

    Map with 5 points, a side pane and buttons to toggle the points of interest

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 //JS for my maps project 
 
+//*** Getting Knockout working
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = "Bert";
-    this.lastName = "Bertington";
-}
+// function AppViewModel() {
+//     this.firstName = "Bert";
+//     this.lastName = "Bertington";
+// }
 
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
-
-
+// // Activates knockout.js
+// ko.applyBindings(new AppViewModel());
 
 
 //js for the map
@@ -22,44 +21,49 @@ function initMap() {
 	// Constructor creates a new map - only center and zoom are required.
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 42.655651, lng: -70.620363},
-	  zoom: 13
+	  zoom: 13,
+	  mapTypeControl:false
 	});
 
-//This array contains ice cream store info in rockport, ma. This info will be displayed to the user
-var locations = [
-	{title: 'The Ice Cream Store', location: {lat: 42.659296, lng: -70.616589}},
-	{title: 'Mollys Sweet Tooth', location: {lat: 42.658429, lng: -70.616801}},
-	{title: 'Robins Nest', location: {lat: 42.658651, lng: -70.617557 }},
-	{title: 'Sundays Ice Cream', location: {lat: 42.658919, lng: -70.616599}},
-	{title: 'Helmut’s Strudel Shop', location: {lat: 42.66045, lng: -70.614633}} 
-	];
+	//This array contains ice cream store info in rockport, ma. This info will be displayed to the user
+	var locations = [
+		{title: 'The Ice Cream Store', location: {lat: 42.659296, lng: -70.616589}},
+		{title: 'Mollys Sweet Tooth', location: {lat: 42.658429, lng: -70.616801}},
+		{title: 'Robins Nest', location: {lat: 42.658651, lng: -70.617557 }},
+		{title: 'Sundays Ice Cream', location: {lat: 42.658919, lng: -70.616599}},
+		{title: 'Helmut’s Strudel Shop', location: {lat: 42.66045, lng: -70.614633}} 
+		];
 
-var largeInfoWindow = new google.maps.InfoWindow();
-var bounds = new google.maps.LatLngBounds();
+	var largeInfoWindow = new google.maps.InfoWindow();
+	//var bounds = new google.maps.LatLngBounds();
 
-//The following group uses the location array to create an array of markers on intialize.
-for(var i=0; i < locations.length; i++){
-	//Get the position from the location array.
-	var position = locations[i].location;
-	var title = locations[i].title;
-	//Create a marker per location, and put into markers array
-	var marker = new google.maps.Marker({
-		map: map,
-		position: position,
-		title: title,
-		animation: google.maps.Animation.DROP,
-		id: i
-	});
-	//Push the marker to our array of markers
-	markers.push(marker);
-	//extend the boundaries of the map for each marker
-	bounds.extend(marker.position);
-	//create an onClick event to open an infowindow at each marker
-	marker.addListener('click', function() {
-		populateInfoWindow(this, largeInfoWindow);
-	});
+	//The following group uses the location array to create an array of markers on intialize.
+	for(var i=0; i < locations.length; i++){
+		//Get the position from the location array.
+		var position = locations[i].location;
+		var title = locations[i].title;
+		//Create a marker per location, and put into markers array
+		var marker = new google.maps.Marker({
+			//map: map,
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			id: i
+		});
+		//Push the marker to our array of markers
+		markers.push(marker);
+		//extend the boundaries of the map for each marker
+		//bounds.extend(marker.position);
+		//create an onClick event to open an infowindow at each marker
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfoWindow);
+		});
+	}
+	//map.fitBounds(bounds);
+
+	document.getElementById('show-listings').addEventListener('click', showListings);
+	document.getElementById('hide-listings').addEventListener('click', hideListings);
 }
-map.fitBounds(bounds);
 //This function populates the infowindow when the marker is clicked. We'll only allow
 // one infoWindow which will open at the marker that is clicked, and populate based
 // on that markers position
@@ -71,11 +75,27 @@ function populateInfoWindow(marker, infowindow) {
 		infowindow.open(map, marker);
 		//Make sure the marker property is cleared if the infoWindow is closed
 		infowindow.addListener('closeclick', function(){
-			infowindow.setMarker(null);
+			infowindow.marker = null;
 		});
 	}
 }
+// This function will loop through the markers array and display them all.
+function showListings() {
+	var bounds = new google.maps.LatLngBounds();
+	// Extend the boundaries of the map for each marker and display the marker
+	for (var i = 0; i < markers.length; i++) {
+	  markers[i].setMap(map);
+	  bounds.extend(markers[i].position);
+	}
+	map.fitBounds(bounds);
+}
 
+// This function will loop through the listings and hide them all.
+function hideListings() {
+	for (var i = 0; i < markers.length; i++) {
+	  markers[i].setMap(null);
+	}
+}
 	// --- Creating a single point on the map
 	// var bearskin = {lat: 42.661047, lng: -70.613833};
 	// var marker = new google.maps.Marker({
@@ -89,4 +109,3 @@ function populateInfoWindow(marker, infowindow) {
 	// marker.addListener('click', function(){
 	// 	infowindow.open(map, marker);
 	// });
-}
